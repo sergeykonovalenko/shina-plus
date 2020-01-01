2c4b120a32253fcd33539c93d2696625a2f176ba
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-01-01 21:20:29 +0200

    Завершил основную десктопную верстку

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -1,6 +1,33 @@
 $(document).ready(function () {
     'use strict';
 
+    // sticky menu
+    let mainHeader = document.querySelector('.main-header');
+    let mainHeaderWrapper = document.querySelector('.main-header__wr');
+    let waypointOffset = 50;
+
+    if (mainHeaderWrapper) {
+        let waypoint = new Waypoint({
+            element: mainHeader,
+            handler: function (direction) {
+                if (direction === 'down') {
+                    mainHeader.style.height = mainHeaderWrapper.offsetHeight + 'px';
+                    mainHeaderWrapper.classList.add('main-header__wr--sticky');
+                    mainHeaderWrapper.classList.remove('main-header__wr--end-sticky');
+                    mainHeaderWrapper.style.top = -mainHeaderWrapper.outerHTML + 'px';
+                } else {
+                    waypointOffset = 0;
+                    mainHeader.style.height = 'auto';
+                    mainHeaderWrapper.classList.remove('main-header__wr--sticky');
+                    mainHeaderWrapper.classList.add('main-header__wr--end-sticky');
+                }
+            },
+            offset: function() {
+                return -(this.element.clientHeight + waypointOffset);
+            }
+        });
+    }
+
     // smooth page scrolling
     $('.scrollto').click(function () {
         var elementClick = '#'+$(this).attr("href").split("#")[1]
---
+++ src/js/vendor/google-maps.js
@@ -0,0 +1,35 @@
+function initMap() {
+
+    let myMap;
+    let element = document.getElementById('google-map');
+
+    let options = {
+        zoom: 17,
+        center: {lat:46.569806, lng:30.791336},
+        scrollwheel: false
+    };
+
+    // maps initialization
+    myMap = new google.maps.Map(element, options);
+
+    // marker and info-window
+    let marker = new google.maps.Marker({
+        position: {lat:46.569806, lng:30.791336},
+        map: myMap,
+        animation: google.maps.Animation.DROP,
+    });
+
+    let InfoWindow = new google.maps.InfoWindow({
+        content: `<div class="info-window">
+                      <img class="info-window__img" src="../img/base/marker-img.jpg" alt="">
+                      <p class="info-window__title">ШинаПлюс</p>
+                      <p class="info-window__subtitle">интернет магазин по продаже шин и дисков в Одессе и Украине</p>
+                      <a class="info-window__phone" href="+380503901419">+38 (050) 390-14-19</a>
+                  </div>`
+    });
+
+    marker.addListener('click', function () {
+        InfoWindow.open(myMap, marker);
+    });
+
+}
